eaea4fb0f5710fc79aad6ab5a1be11fba40f0e3f
Suryansh Gupta <suryanshgupta233@gmail.com>
2026-04-16 18:19:17 +0530

    final fix server path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const schoolRoutes = require("./routes/schoolRoutes");
+const schoolRoutes = require("./routes/schoolRoutes"); 
 
 const app = express();
 
@@ -11,25 +11,8 @@ app.use(bodyParser.json());
 
 app.use("/api", schoolRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-const schoolRoutes = require("./routes/schoolRoutes");
-
-const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
-
-app.use("/api", schoolRoutes);
-
-const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
