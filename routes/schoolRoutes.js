31142a486a35b0d38064c31819d78636a1e3fd97
Suryansh Gupta <suryanshgupta233@gmail.com>
2026-04-16 16:38:33 +0530

    final fix

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const{
-    addSchool,
-    listSchools,
-} = require("../controllers/schoolcontroller");
+const { addSchool, listSchools } = require("../controllers/schoolController");
 
 router.post("/addSchool",addSchool);
 router.get("/listSchools", listSchools);
